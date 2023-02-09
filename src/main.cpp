c48e8e0a024658739c39e192386d2c5c62512a8d
Benjamin Parsons-Willis <ben@vyrz.dev>
2023-02-09 16:50:59 +0000

    Removed costly printfs

--- src/main.cpp
+++ src/main.cpp
@@ -36,12 +36,12 @@ int main() {
 
         if (pre_filter_buf_cursor == 8) {
             if (!lag_filter_enable) {
-                printf("Waiting for lag filter.");
+                // printf("Waiting for lag filter.");
                 // Push to tail.
                 post_filter_buf[post_filter_buf_tail] = input;
                 post_filter_buf_tail++;
             } else {
-                printf("Starting Lag Filter\n");
+                // printf("Starting Lag Filter\n");
                 // Do bounds-check.
                 if (post_filter_buf_tail >= TREND_REMOVAL_INIT_LENGTH) {
                     post_filter_buf_tail = 0;
@@ -65,10 +65,10 @@ int main() {
                 post_filter_buf[post_filter_buf_tail] = avg;
                 post_filter_buf_tail++;
             }
-            printf("Finished Lag Filter.\n");
+            // printf("Finished Lag Filter.\n");
             // Only true when buf has 128; Can now do rolling avg.
             if (post_filter_buf_tail == post_filter_buf_head) {
-                printf("Starting rolling avg.\n");
+                // printf("Starting rolling avg.\n");
                 unsigned int avg = 0;
                 unsigned char cursor = 0;
                 while (cursor < TREND_REMOVAL_INIT_LENGTH) {
@@ -80,7 +80,7 @@ int main() {
                     offset = avg - post_filter_buf[post_filter_buf_tail];
                 }
 
-                printf("%d %d %d %d\n", offset, avg, post_filter_buf_tail, post_filter_buf_head);
+                // printf("%d %d %d %d\n", offset, avg, post_filter_buf_tail, post_filter_buf_head);
                 out.write_u16(post_filter_buf[post_filter_buf_tail] - avg + offset);
             } else {
                 out.write_u16(32768);
